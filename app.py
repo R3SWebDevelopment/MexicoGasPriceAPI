7a01572c02725e4aef7721d2f74e063dff4fc867
R3S <ricardo.tercero@r3s.com.mx>
2023-04-07 16:16:57 -0600

    Add wsgi.py

--- app.py
+++ app.py
@@ -7,3 +7,7 @@ app = Flask(__name__)
 def health_check():
     response = f"<p>request.remote_addr: {request.remote_addr}</p>"
     return response
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
